7a5fee31356e7e382ae89d19c616a2d76e96eeff
wolfymmm <marusina.yana@lll.kpi.ua>
2026-05-11 00:22:25 +0300

    fixed routes

--- backend/src/routes/bookRoutes.ts
+++ backend/src/routes/bookRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { getBooks, getBookById, createBook, updateBook, deleteBook } from '../controllers/book.controller.js';
+import { getBooks, getBookById, createBook, updateBook, deleteBook, analyzeBookAI } from '../controllers/book.controller.js';
 import { protect } from '../middleware/authMiddleware.js';
 import { adminOnly } from '../middleware/adminMiddleware.js';
 
 const router: Router = Router();
 
+
+router.post('/analyze-ai', analyzeBookAI);
 // GET /api/books - отримати всі книги
 router.get('/', getBooks);
 // GET /api/books/:id - отримати одну книгу за ID
@@ -14,6 +16,6 @@ router.post('/', protect, adminOnly, createBook);
 router.put('/:id', protect, adminOnly, updateBook);
 router.delete('/:id', protect, adminOnly, deleteBook);
 
-router.post('/analyze-ai', analyzeBookAI);
+
 
 export default router;
